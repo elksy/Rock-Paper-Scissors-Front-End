8f63135c40f37b399f92c20726ccd79734bd610a
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-21 14:51:51 +0000

    React Routing done

--- rps-app/src/App.js
+++ rps-app/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import Lobby from "./Components//Lobby/Lobby.js";
 import "./App.css";
+import Lobby from "./Components//Lobby/Lobby.js";
 import CreateTournament from "./Components/CreateTournament/CreateTournament.js";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Lobby />
-        <CreateTournament />
-      </div>
+      <Switch>
+        <Route path="/lobby">
+          <Lobby />
+        </Route>
+        <Route path="/create-tournament">
+          <CreateTournament />
+        </Route>
+      </Switch>
     );
   }
 }
