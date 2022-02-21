b98ec1e0871d62656171e8b6cc09464151a65bea
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-21 13:06:47 +0000

    create tournament page css placeholder done

--- rps-app/src/Components/CreateTournament/CreateTournament.js
+++ rps-app/src/Components/CreateTournament/CreateTournament.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./createtournament.css";
 
 class CreateTournament extends React.Component {
   render() {
     return (
       <div className="create-tournament-page">
-        <header>Create Tournament</header>
+        <header>
+          <h1>Create Tournament</h1>
+        </header>
         <div className="num-rounds">
-          <h5>Number of Rounds</h5>
+          <h3>Number of Rounds</h3>
           <div className="rounds-btns">
             <button type="submit">1</button>
             <button type="submit">3</button>
