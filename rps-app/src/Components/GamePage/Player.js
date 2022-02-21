f64f3c38b5a00b558115cac8561b7c0eaf7320b9
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-21 16:23:36 +0000

    Added page to display a game of rock, paper, scissors

---
+++ rps-app/src/Components/GamePage/Player.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ChoiceMade from "./ChoiceMade.js";
+import rock from ".//images/rock.png";
+import paper from ".//images/paper.png";
+import scissors from ".//images/scissors.png";
+
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selection: "" };
+  }
+
+  updateSelection = (word) => {
+    this.setState({ selection: word });
+  };
+
+  render() {
+    return (
+      <div className="game">
+        <h2>username</h2>
+        {/* Display the players choice once they have made a selection */}
+        <ChoiceMade selection={this.state.selection} />
+        <div className="selection-wrapper">
+          <div
+            className="selection-div"
+            onClick={() => this.updateSelection("rock")}
+          >
+            <img className="selection-image" src={rock} alt="rock" />
+          </div>
+          <div
+            className="selection-div"
+            onClick={() => this.updateSelection("paper")}
+          >
+            <img className="selection-image" src={paper} alt="paper" />
+          </div>
+          <div
+            className="selection-div"
+            onClick={() => this.updateSelection("scissors")}
+          >
+            <img className="selection-image" src={scissors} alt="scissors" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Player;
---
+++ rps-app/src/Components/GamePage/Timer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Timer extends React.Component {
+  render() {
+    return (
+      <div className="timer">
+        {/* Need to finds a way to display the timer */}
+        <h1>Time Left</h1>
+      </div>
+    );
+  }
+}
+
+export default Timer;
