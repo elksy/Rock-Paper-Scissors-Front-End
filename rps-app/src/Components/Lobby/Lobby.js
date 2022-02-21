055c2cfcef169f23da0c1bd8d482a7bffe2ba5bb
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-21 12:10:45 +0000

    Added folders to structure the files

---
+++ rps-app/src/Components/Lobby/Lobby.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./lobby.css";
+
+class Lobby extends React.Component {
+  render() {
+    return (
+      <div className="lobby">
+        <div>Hello Lobby</div>
+        <div>Player List</div>
+        <div>Chat</div>
+        <div>Buttons</div>
+      </div>
+    );
+  }
+}
+export default Lobby;
