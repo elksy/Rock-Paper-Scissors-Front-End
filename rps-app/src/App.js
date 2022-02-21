e7c3e1b4256ab4f5e29a781a416cedaf9d6bb0a0
rob.elkes@btinternet.com <rob.elkes@btinternet.com>
2022-02-21 11:15:52 +0000

    created initial react app

---
+++ rps-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return <div className="App">Hello World</div>;
+  }
+}
+
+export default App;
