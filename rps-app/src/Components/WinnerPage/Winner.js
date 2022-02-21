aeedab8c9953f0141663ab2b3829bc6707f6a673
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-21 17:29:14 +0000

    added redirects to different paths

--- rps-app/src/Components/WinnerPage/Winner.js
+++ rps-app/src/Components/WinnerPage/Winner.js
@@ -3,7 +3,18 @@ import "./winner.css";
 
 class Winner extends React.Component {
   render() {
-    return <div className="winner-page">Winner Page</div>;
+    return (
+      <div className="winner-page">
+        <h1>Winner Page</h1>
+        <div>Winners area</div>
+        <div className="chat-comp">Chat component</div>
+        <div className="winner-page-btns">
+          <button input="button">Play Again</button>
+          <button input="button">Leave</button>
+          <button input="button">End Tournament</button>
+        </div>
+      </div>
+    );
   }
 }
 
