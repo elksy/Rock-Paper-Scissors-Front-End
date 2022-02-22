39368448ab5d5bf191709bd334d8755f8f4af33c
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-22 16:10:36 +0000

    Play Game modal connected

--- rps-app/src/Components/LandingPage/LandingPage.js
+++ rps-app/src/Components/LandingPage/LandingPage.js
@@ -1,22 +1,75 @@
 import React from "react";
 import "./LandingPage.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  handleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
   render() {
     return (
-      <div className="landing-page-container">
-        <h1>Rock, Paper, Scissors</h1>
+      <div>
+        <Modal
+          show={this.state.showModal}
+          onHide={() => {
+            this.handleModal();
+          }}
+          class="modal-dialog modal-dialog-centered"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Enter Nickname</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form className="form" /* onSubmit={(e) => this.handleSubmit(e)} */>
+              <Form.Group className="mb-3">
+                <Form.Label className="username-label">Username</Form.Label>
+                <Form.Control
+                  className="username-input"
+                  type="text"
+                  placeholder="Enter nickname"
+                  // value={this.state.username}
+                  id="email"
+                  // onChange={(e) => this.handleChange(e)}
+                />
+              </Form.Group>
+              <Button className="example" variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+        <div>
+          <div className="landing-page-container">
+            <h1>Rock, Paper, Scissors</h1>
 
-        <div className="menu-button-container">
-          <button type="submit" className="menu-button">
-            Play Game
-          </button>
-          <button type="submit" className="menu-button">
-            Join Tournament
-          </button>
-          <button type="submit" className="menu-button">
-            Create Tournament
-          </button>
+            <div className="menu-button-container">
+              <Button
+                type="submit"
+                className="btn btn-primary"
+                onClick={() => {
+                  this.handleModal();
+                }}
+              >
+                Play Game
+              </Button>
+              <Button type="submit" className="btn btn-primary">
+                Join Tournament
+              </Button>
+              <Button type="submit" className="btn btn-primary">
+                Create Tournament
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     );
