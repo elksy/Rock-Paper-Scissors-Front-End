c6c49d7600213f0846a34b2cd8611c76142f5326
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-22 14:38:35 +0000

    progress of modal

--- rps-app/src/Components/LandingPage/LandingPage.js
+++ rps-app/src/Components/LandingPage/LandingPage.js
@@ -2,23 +2,23 @@ import React from "react";
 import "./LandingPage.css";
 
 export default class LandingPage extends React.Component {
-	render() {
-		return (
-			<div className="landing-page-container">
-				<h1>Rock, Paper, Scissors</h1>
+  render() {
+    return (
+      <div className="landing-page-container">
+        <h1>Rock, Paper, Scissors</h1>
 
-				<div className="menu-button-container">
-					<button type="submit" className="menu-button">
-						Play Game
-					</button>
-					<button type="submit" className="menu-button">
-						Join Tournament
-					</button>
-					<button type="submit" className="menu-button">
-						Create Tournament
-					</button>
-				</div>
-			</div>
-		);
-	}
+        <div className="menu-button-container">
+          <button type="submit" className="menu-button">
+            Play Game
+          </button>
+          <button type="submit" className="menu-button">
+            Join Tournament
+          </button>
+          <button type="submit" className="menu-button">
+            Create Tournament
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
