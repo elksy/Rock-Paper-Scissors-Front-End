2860704c3d314f75a114b86cf1121aa3cbce2be4
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-23 11:09:14 +0000

    pushing winner page again

--- rps-app/src/Components/WinnerPage/Winner.js
+++ rps-app/src/Components/WinnerPage/Winner.js
@@ -1,9 +1,31 @@
 import React from "react";
 import "./winner.css";
+import { Button } from "react-bootstrap";
 
 class Winner extends React.Component {
   render() {
-    return <div className="winner-page">Winner Page</div>;
+    return (
+      <div className="winner-page">
+        <header className="header">
+          <h1>Tournament Complete!</h1>
+        </header>
+        <main>
+          <div className="winner-comps">
+            <div className="winners-div">
+              <div className="winners-area">Winners Podium</div>
+              <div className="losers-area">Losers!</div>
+            </div>
+
+            <div className="chat-comp">Chat</div>
+          </div>
+          <div className="winner-page-btns">
+            <Button className="btn-primary">Play Again</Button>
+            <Button input="button">Leave</Button>
+            <Button input="button">End Tournament</Button>
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
