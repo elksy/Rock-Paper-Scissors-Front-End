73f10708b1580949a4f2d13d3682774ce2e36bec
Mariam01010010 <mariamraashid@hotmail.com>
2022-02-25 12:39:37 +0000

    colour picker done and state accepts change

--- rps-app/src/Components/LandingPage/ColourForm.js
+++ rps-app/src/Components/LandingPage/ColourForm.js
@@ -1,37 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
+import { CirclePicker } from "react-color";
 
 class ColourForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playerColour: undefined,
+      playerColour: "#fff",
     };
   }
 
-  handleColourChange = (e) => {
-    this.setState({ playerColour: e.target.value });
-    this.disableButton();
-  };
-
-  disableButton = () => {
-    if (this.state.playerName.length > 2) {
-      this.props.updateDisabledButton(false);
-    } else {
-      this.props.updateDisabledButton(true);
-    }
+  handleColourChange = (colour) => {
+    this.setState({ playerColour: colour.hex });
   };
 
   render() {
     return (
       <Form.Group className="mb-3">
         <Form.Label className="colour-label">Choose a colour</Form.Label>
-        <Form.Control
+        <CirclePicker
           className="colour-input"
-          type="color"
-          value={this.state.playerColour}
           id="colour"
-          onChange={(e) => this.handleColourChange(e)}
+          color={this.state.playerColour}
+          onChange={this.handleColourChange}
+          value={this.state.playerColour}
         />
       </Form.Group>
     );
