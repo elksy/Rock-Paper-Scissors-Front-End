73c6ed38dd839e17064ad25610bed01c74dad530
Mariam01010010 <mariamraashid@hotmail.com>
2022-03-02 12:28:11 +0000

    saving changes made for podium

---
+++ rps-app/src/Components/WinnerPage/Podium/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: "1tfjiELNrwYAJeafRYlT9RwOIiD",
+    name: "Grace Hopper",
+  },
+  {
+    id: "1tfjiFoinFrbdLWlPI52dRLhNlD",
+    name: "Yoshitake Miura",
+  },
+  {
+    id: "1tfjiDIAS8f2UYgV9ynCqWi7rZD",
+    name: "Ada Lovelace",
+  },
+  {
+    id: "1tfjiEIWBZz2I9lOQYTEeMICALg",
+    name: "Grete Hermann",
+  },
+  {
+    id: "1tfjiCMU9SdFM9BAaIF3mS5UpYf",
+    name: "Chieko Asakawa",
+  },
+].map((winner, position) => ({ ...winner, position }));
