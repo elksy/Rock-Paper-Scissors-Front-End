646bb3c0b19738f68f056ac3ac7a30ba68c4b25c
Senre <kishorshris@gmail.com>
2022-03-03 15:00:25 +0000

    barred users from certain endpoints when no sessionId

--- rps-app/src/tests/App.test.js
+++ rps-app/src/tests/App.test.js
@@ -25,6 +25,5 @@ test("Landing page has buttons to play game and, join and create a tournament", 
     </Router>
   );
   screen.getByRole("button", { name: "Play Game" });
-  screen.getByRole("button", { name: "Join Tournament" });
   screen.getByRole("button", { name: "Create Tournament" });
 });
